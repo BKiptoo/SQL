d422dc4157007d0832298d5568be1e9f3fee7eaa
BKiptoo <bettsonk@gmail.com>
2025-08-18 11:08:28 +0300

    Add student data insertion and update phone column type in create.sql; create read.sql for student queries

---
+++ postgres/CRUD/read.sql
@@ -0,0 +1,14 @@
+-- Read all columms
+-- SELECT * FROM students 
+
+-- Read ony name and email
+
+-- SELECT name,email FROM students;
+
+-- SELECT * from students
+-- where email='john@mail.com'
+-- OR
+-- email = 'janedoe@example.com';
+
+SELECT * from students
+ORDER BY name ASC;
